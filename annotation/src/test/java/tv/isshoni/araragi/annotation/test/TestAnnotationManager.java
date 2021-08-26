5506750cb99085227596d0b809731ec86c26a4ed
Andrew Burr <me@dragovorn.com>
2021-08-25 22:59:01 -0400

    expand test code

--- annotation/src/test/java/tv/isshoni/araragi/annotation/test/TestAnnotationManager.java
+++ annotation/src/test/java/tv/isshoni/araragi/annotation/test/TestAnnotationManager.java
@@ -10,21 +10,44 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestAnnotationManager {
 
+    public static final String EXPECTED_VALUE = "EXPECT ME";
+
+    private OutputStream output;
+
+    @Before
+    public void before() {
+        this.output = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(this.output));
+    }
+
     @Test
-    public void testAnnotationManager() {
-        OutputStream stream = new ByteArrayOutputStream();
-
-        System.setOut(new PrintStream(stream));
-
+    public void testAnnotationManagerProcessorConstructors() {
         AnnotationManager annotationManager = new AnnotationManager();
 
         annotationManager.discoverAnnotation(TestAnnotation.class);
         annotationManager.discoverAnnotation(Second.class);
 
-        assertEquals("Annotation\n", stream.toString());
+        assertEquals(EXPECTED_VALUE, this.output.toString());
+    }
+
+    @Test
+    public void testAnnotationManagerMagicMethod() throws Exception {
+        AnnotationManager annotationManager = new AnnotationManager();
+
+        annotationManager.discoverAnnotation(TestAnnotation.class);
+
+        annotationManager.execute(TestAnnotationManager.class.getMethod("aMethod", String.class), this);
+
+        assertEquals(EXPECTED_VALUE, this.output.toString());
+    }
+
+    public void aMethod(@TestAnnotation(EXPECTED_VALUE) String str) {
+        System.out.print(str);
     }
 }
--- annotation/src/test/java/tv/isshoni/araragi/annotation/test/model/SecondAnnotationProcessor.java
+++ annotation/src/test/java/tv/isshoni/araragi/annotation/test/model/SecondAnnotationProcessor.java
@@ -1,11 +1,12 @@
 package tv.isshoni.araragi.annotation.test.model;
 
 import tv.isshoni.araragi.annotation.model.IAnnotationProcessor;
+import tv.isshoni.araragi.annotation.test.TestAnnotationManager;
 
 public class SecondAnnotationProcessor implements IAnnotationProcessor<Second> {
 
-    public SecondAnnotationProcessor(@TestAnnotation("Annotation") String str) {
+    public SecondAnnotationProcessor(@TestAnnotation(TestAnnotationManager.EXPECTED_VALUE) String str) {
         System.err.println("--- Construct SecondAnnotationProcessor ---");
-        System.out.println(str);
+        System.out.print(str);
     }
 }
