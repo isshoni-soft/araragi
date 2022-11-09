f55cb70064602cdc5265d1bec446d755d27bd85c
Andrew <me@dragovorn.com>
2022-11-09 16:14:51 -0500

    bump version & upstream some reflection helpers from winry.

--- core/src/main/java/tv/isshoni/araragi/reflect/ReflectionUtil.java
+++ core/src/main/java/tv/isshoni/araragi/reflect/ReflectionUtil.java
@@ -6,12 +6,66 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Executable;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ReflectionUtil {
+
+    public static <T> T execute(Class<?> from, Object target, String methodName, Object... parameters) {
+        try {
+            return (T) from.getMethod(methodName, convertObjectsToClass(parameters)).invoke(target, parameters);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T construct(Class<T> clazz, Object... parameters) {
+        try {
+            return construct(clazz.getConstructor(convertObjectsToClass(parameters)), parameters);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T construct(Constructor<T> constructor, Object... parameters) {
+        try {
+            return constructor.newInstance(parameters);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void injectField(Field field, Object target, Object injected) {
+        boolean couldAccess = field.canAccess(target);
+
+        if (!couldAccess) {
+            field.setAccessible(true);
+        }
+
+        try {
+            field.set(target, injected);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        if (!couldAccess) {
+            field.setAccessible(false);
+        }
+    }
+
+    public static Class<?>[] convertObjectsToClass(Object... objects) {
+        Class<?>[] result = new Class<?>[objects.length];
+
+        for (int x = 0; x < objects.length; x++) {
+            result[x] = objects[x].getClass();
+        }
+
+        return result;
+    }
 
     @SafeVarargs
     public static <T> List<Constructor<T>> discoverAnnotatedConstructors(Class<T> clazz, Class<? extends Annotation>... annotations) {
