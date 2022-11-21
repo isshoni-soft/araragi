e1f3abd1c6ce887b44d4c9bb14098a4a07b3d497
Andrew <me@dragovorn.com>
2022-11-21 08:35:44 -0500

    create ComparatorUtil

---
+++ core/src/main/java/tv/isshoni/araragi/util/ComparatorUtil.java
@@ -0,0 +1,21 @@
+package tv.isshoni.araragi.util;
+
+import java.util.function.Predicate;
+
+public final class ComparatorUtil {
+
+    public static <T> int simpleCompare(T f, T s, Predicate<T> comparator) {
+        boolean fResult = comparator.test(f);
+        boolean sResult = comparator.test(s);
+
+        if (fResult && sResult) {
+            return 0;
+        } else if (sResult) {
+            return 1;
+        } else if (fResult) {
+            return -1;
+        }
+
+        return 2;
+    }
+}
