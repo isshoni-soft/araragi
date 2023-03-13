b6521742b48bae36e2eb795852221cfb5fbf7153
Andrew <me@dragovorn.com>
2023-03-12 23:01:43 -0400

    make replacement changable in StringToken

--- core/src/main/java/tv/isshoni/araragi/string/format/StringToken.java
+++ core/src/main/java/tv/isshoni/araragi/string/format/StringToken.java
@@ -2,18 +2,20 @@ package tv.isshoni.araragi.string.format;
 
 public class StringToken {
 
-    private static final int TOKEN_DISCRIMINATOR_LENGTH = 3;
-
     private final int start;
     private final int finish;
 
     private final String key;
-    private final String replacement;
+    private String replacement;
 
     public StringToken(int start, int finish, String key, String replacement) {
         this.start = start;
         this.finish = finish;
         this.key = key;
+        this.replacement = replacement;
+    }
+
+    public void setReplacement(String replacement) {
         this.replacement = replacement;
     }
 
@@ -36,4 +38,15 @@ public class StringToken {
     public String getKey() {
         return this.key;
     }
+
+    // TODO - Implement these.
+//    @Override
+//    public int hashCode() {
+//        return super.hashCode();
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        return super.equals(obj);
+//    }
 }
