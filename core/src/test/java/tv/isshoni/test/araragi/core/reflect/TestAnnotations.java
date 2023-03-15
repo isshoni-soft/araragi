19bc4cbd2fb08351bf0ffb8755559d966696af3e
Andrew <me@dragovorn.com>
2023-03-15 08:43:51 -0400

    comment out test that doesn't do anything

--- core/src/test/java/tv/isshoni/test/araragi/core/reflect/TestAnnotations.java
+++ core/src/test/java/tv/isshoni/test/araragi/core/reflect/TestAnnotations.java
@@ -1,6 +1,5 @@
 package tv.isshoni.test.araragi.core.reflect;
 
-import org.junit.Test;
 import tv.isshoni.test.araragi.model.TestAnno;
 
 public class TestAnnotations {
@@ -8,8 +7,8 @@ public class TestAnnotations {
     @TestAnno("testing")
     private String field;
 
-    @Test
-    public void testAliasFor() {
-
-    }
+//    @Test
+//    public void testAliasFor() {
+//
+//    }
 }
