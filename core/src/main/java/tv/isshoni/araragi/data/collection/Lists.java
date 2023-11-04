9d10784e08b8d5d2b57935861f1eb2fbff8219dd
Andrew <me@dragovorn.com>
2023-11-04 10:29:42 -0400

    add newArray to araragi and bump version

--- core/src/main/java/tv/isshoni/araragi/data/collection/Lists.java
+++ core/src/main/java/tv/isshoni/araragi/data/collection/Lists.java
@@ -1,12 +1,34 @@
 package tv.isshoni.araragi.data.collection;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 
 public final class Lists {
 
+    public static <T> T[] newArray(Class<T> clazz, int capacity) {
+        return (T[]) Array.newInstance(clazz, capacity);
+    }
+
     @SafeVarargs
     public static <T> LinkedList<T> linkedListOf(T... values) {
         return new LinkedList<>(Arrays.asList(values));
+    }
+
+    @SafeVarargs
+    public static <T> ArrayList<T> arrayListOf(T... values) {
+        return new ArrayList<>(Arrays.asList(values));
+    }
+
+    @SafeVarargs
+    public static <T> T[] arrayOf(Class<T> clazz, T... values) {
+        T[] result = newArray(clazz, values.length);
+
+        for (int x = 0; x < values.length; x++) {
+            result[x] = values[x];
+        }
+
+        return result;
     }
 }
